2d6996174ea6e5327cbc4f279c49ee964b1f1e31
suruozhong <839293390@qq.com>
2020-05-23 20:36:59 +0800

    留外网host

--- src/constant/config.js
+++ src/constant/config.js
@@ -1,7 +1,7 @@
 /* eslint-disable */
 module.exports = {
-    host: 'http://localhost:8900/zhong/front',
-    // host: 'https://lawyer.newmonth.cn/zhong/front',
+    // host: 'http://localhost:8900/zhong/front',
+    host: 'https://lawyer.newmonth.cn/zhong/front',
     token: '你的token',
     name: '小助手'
 }
