7797d57e11d734e0d06ec422ed8770fecab50c74
suruozhong <839293390@qq.com>
2020-05-23 19:52:58 +0800

    bug

--- src/constant/config.js
+++ src/constant/config.js
@@ -2,6 +2,6 @@
 module.exports = {
     host: 'http://localhost:8900/zhong/front',
     // host: 'https://lawyer.newmonth.cn/zhong/front',
-    token: 'puppet_padplus_4557e53a402c82fe',
+    token: '你的token',
     name: '小助手'
 }
