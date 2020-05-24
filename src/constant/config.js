4cd629f793982d3d485332820c037d12a09fec9c
suruozhong <839293390@qq.com>
2020-05-24 13:01:36 +0800

    bug

--- src/constant/config.js
+++ src/constant/config.js
@@ -1,6 +1,6 @@
 /* eslint-disable */
 module.exports = {
-    host: 'http://localhost:8900/zhong/front',
+    host: 'https://lawyer.newmonth.cn/zhong/front',
     token: 'puppet_padplus_4557e53a402c82fe',
     name: '团团'
 }
